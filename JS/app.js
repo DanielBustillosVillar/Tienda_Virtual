018306c6c5da36a03b1dffb6ab568d2d8bb644c2
DanielBustillosVillar <68455232+DanielBustillosVillar@users.noreply.github.com>
2021-05-21 19:46:57 -0500

    Terminé la página electrohogar

--- JS/app.js
+++ JS/app.js
@@ -1,14 +1,41 @@
 /* =========== VARIABLES Y CONSTANTES ============*/
 const menu__movil = document.querySelector('#menu__movil')
 const navegacion = document.querySelector('#navegacion')
+
+const slider = document.querySelector('#banner_contenido')
+let sliderImg = document.querySelectorAll('#banner_contenido img')
+let sliderImgLast = sliderImg[sliderImg.length - 1]
+
+
+slider.insertAdjacentElement('afterbegin', sliderImgLast)
 /* =================== EVENTOS ============================= */
+
 menu__movil.addEventListener('click', salirMenu)
+/* =================== SLIDER =================== */
+window.addEventListener('load', mover)
+
+
 
 /* ================= FUNCTIONES ====================== */
+
+function mover(){
+    setInterval(moverDerecha, 3000)
+}
 function salirMenu(){
     if(navegacion.classList.contains('left')){
         navegacion.classList.remove('left')
     }else{
         navegacion.classList.add('left')
     }
+}
+
+function moverDerecha(){
+    let sliderImgFirst = document.querySelectorAll('#banner_contenido img')[0]
+    slider.style.marginLeft = "-200%"
+    slider.style.transition = "all 0.5s"
+    setTimeout(function(){
+        slider.style.transition = "none"
+        slider.insertAdjacentElement('beforeend', sliderImgFirst)
+        slider.style.marginLeft = "-100%"
+    }, 500)
 }
